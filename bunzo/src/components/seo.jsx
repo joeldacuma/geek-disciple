00e67c1a148850d0b7ccc38c251fe305a57587b8
Joel Dacuma <>
2022-05-14 15:51:08 +0800

    clean seo.jsx

--- bunzo/src/components/seo.jsx
+++ bunzo/src/components/seo.jsx
@@ -45,13 +45,8 @@ const SEO = ({
     const language = lang || site.siteMetadata.siteLanguage;
     const siteUrl = site.siteMetadata.siteUrl.replace(/\/$/, "");
     const mainUrl = site.siteMetadata.mainUrl.replace(/\/$/, "");
-    const bannerImage =
-        metaImage && metaImage.images.fallback.src
-            ? `${mainUrl}${metaImage.images.fallback.src}`
-            : `${siteUrl}/${site.siteMetadata.image}`;
+    const bannerImage =  metaImage.images.fallback.src;
 
-    const imgWidth = 875;
-    const imgHeight = 554;
     pageUrl = `${siteUrl}/${path}`;
     pageUrl = pageUrl.replace(/^\/+/g, "");
     console.log(pageUrl);
@@ -64,39 +59,6 @@ const SEO = ({
     } else {
         siteTitle = `${metaTitle}`;
     }
-
-    const basSchema = [
-        {
-            "@type": "Organization",
-            "@id": `${siteUrl}/#organization`,
-            name: `${siteTitle}`,
-            url: siteUrl,
-            logo: {
-                "@type": "ImageObject",
-                url: site.siteMetadata.logo,
-            },
-        },
-        {
-            "@type": "WebSite",
-            "@id": `${siteUrl}/#website`,
-            url: siteUrl,
-            name: `${siteTitle}`,
-            publisher: {
-                "@id": `${siteUrl}/#organization`,
-            },
-            inLanguage: language,
-            potentialAction: {
-                "@type": "SearchAction",
-                target: `${siteUrl}/?s={search_term_string}`,
-                "query-input": "required name=search_term_string",
-            },
-        },
-    ];
-
-    const schemaOrgWebPage = {
-        "@context": "http://schema.org",
-        "@graph": [...basSchema],
-    };
 
     return (
         <Helmet
@@ -113,36 +75,23 @@ const SEO = ({
                 name="robots"
                 content="index, follow, max-snippet:-1, max-video-preview:-1, max-image-preview:large"
             />
-            {prevLink && <link rel="prev" href={prevLink} />}
-            {nextLink && <link rel="next" href={nextLink} />}
 
             {/* OpenGraph tags */}
             <meta property="og:locale" content={language} />
-            {isBlogPost ? <meta property="og:type" content="article" /> : null}
+            <meta property="og:type" content="article" />
             <meta property="og:type" content="website" />
             <meta property="og:url" content={pageUrl} />
             <meta property="og:title" content={siteTitle} />
             <meta property="og:description" content={metaDescription} />
             <meta property="og:image" content={bannerImage} />
-            <meta property="og:image:secure_url" content={bannerImage} />
-            <meta property="og:image:width" content={`${imgWidth}px`} />
-            <meta property="og:image:height" content={`${imgHeight}px`} />
-            <meta property="og:image:alt" content={siteTitle} />
-            <meta property="og:image:type" content="image/png" />
+            <meta property="og:image:width" content="500" />
+            <meta property="og:image:height" content="500" />
 
             {/* Twitter Card tags */}
             <meta name="twitter:card" content="summary_large_image" />
-            <meta
-                name="twitter:creator"
-                content={site.siteMetadata.twitterUsername}
-            />
             <meta name="twitter:title" content={siteTitle} />
             <meta name="twitter:description" content={metaDescription} />
             <meta name="twitter:image" content={bannerImage} />
-
-            <script type="application/ld+json">
-                {JSON.stringify(schemaOrgWebPage)}
-            </script>
         </Helmet>
     );
 };
